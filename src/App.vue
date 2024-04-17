133a0fd644951ca536c3eccb6ebf8012fa5104b0
Federica Gallo <gallofederica58@gmail.com>
2024-04-17 12:41:27 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,25 @@
 </template>
 
 <script>
+import {store} from './store.js';
+import axios from 'axios';
   export default {
-    name: 'App'
+    name: 'App',
+    data(){
+      return {
+        store,
+      }
+    },
+    methods: {
+      getTv(){
+        axios.get(this.store.endPoints.apiUrl + this.store.endPoints.movie, {params: this.store.options.params }).then((res) => 
+      this.store.tvShow = res.data);
+      console.log(this.store.tvShow)
+      }
+    },
+    created(){
+      this.getTv()
+    }
   }
 </script>
 
